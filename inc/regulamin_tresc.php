a6d1dbb68d1cddea0f5f7b3ae7249919274e60d3
Sławomir <47289398+goodstat@users.noreply.github.com>
2019-02-04 09:56:14 +0100

    Add files via upload

---
+++ inc/regulamin_tresc.php
@@ -0,0 +1,48 @@
+				<ol type='1'>
+						
+						<div class="page-header">
+							<h2>Warunki korzystania</h2>
+						</div>
+						
+						<li>System GoodStat jest całkowicie darmowy, lecz dopuszcza się korzystanie z niego wszelkim instytucjom i podmiotom prowadzących działalność gospodarczą, jak i osobom prywatntym które takiej działalności nie prowadzą.</li>
+						<li>System GoodStat obejmuje dowolną liczbę domen/instalacji.</li>
+						<li>Korzystając z Systemu GoodStat zobowiązujesz się do nie narażania serwisu http://goodstat.com.pl/ na straty, mowa tutaj o wszystkich aspektach związanych z funkcjonalnością serwisu.</li>
+						<li>Poniższe typy stron nie są wspierane i nie będą tolerowane przez System GoodStat:
+							<ol type='a'>
+								<li>strony promujące rasizm i przemoc, lub jakąkolwiek inną nielegalną działalność, w jakiejkolwiek formie</li>
+								<li>strony zawierające informacje, materiały lub dyskusje prowadzone w nielegalnej działalności</li>
+								<li>strony promujące lub wykorzystujące oprogramowanie zaprojektowany do dostarczania niechcianej korespondencji email</li>
+								<li>strony zawierające dziecięcą pornografię, albo jakiekolwiek inne, które ogólnie uznawane są za niestosowne</li>								
+							</ol>
+						</li>
+						
+						<div class="page-header">
+							<h2>Odpowiedzialność</h2>
+						</div>
+
+						<li>Autor nie odpowiada za jakiekolwiek straty wynikłe z używania Systemu GoodStat, w tym utratę spodziewanych korzyści, danych, informacji gospodarczych lub koszt urządzeń lub programów zastępczych.</li>
+						<li>Nabywca bierze na siebie ryzyko wynikające z użytkowania Systemu GoodStat.</li>
+						<li>Autor nie udziela żadnej gwarancji oraz rękojmi, że System GoodStat będzie działać na wszystkich serwerach.</li>
+					
+						<div class="page-header">
+							<h2>Ochrona danych osobowych</h2>
+						</div>
+
+						<li>Wszelkie wprowadzone dane nabywcy podczas instalacji Systemu GoodStat, nigdzie nie są i nigdy nie będą przechowywane przez autora Systemu GoodStat, są one zbierane jedynie w celu prawidłowej instalacji Systemu GoodStat.</li>
+					
+						<div class="page-header">
+							<h2>Dostawa</h2>
+						</div>
+
+						<li>Dostawa Systemu GoodStat następuje drogą elektroniczną  poprzez udostępnienie Użytkownikowi linku do ściągnięcia kopii wersji Systemu GoodStat.</li>
+						<li>Serwis http://goodstat.com.pl/ nie oferuje programów zapisanych na nośnikach materialnych.</li>
+					
+						<div class="page-header">
+							<h2>Postanowienia końcowe</h2>
+						</div>
+
+						<li>Nabywca System GoodStat powinien zapoznać się z treścią Regulaminu najpóźniej w chwili instalacji Systemu GoodStat.</li>
+						<li>Ewentualne zmiany Regulaminu będą dokonywane wyłącznie z ważnego powodu i zostaną opublikowane na stronie www.goodstat.com.pl w zakładce „Regulamin”.</li>
+						<li>Regulamin został sporządzony na podstawie obowiązujących przepisów prawa polskiego. W sprawach nieuregulowanych Regulaminem, a także w zakresie w jakim Regulamin okazałby się sprzeczny z bezwzględnie obowiązującymi przepisami prawa, stosuje się przepisy prawa bezwzględnie obowiązującego.</li>
+						<li>Regulamin dostępny jest dla wszystkich Klientów w wersji elektronicznej na stronie goodstat.com.pl.</li>
+				</ol>
---
+++ inc/stopka_instal.php
@@ -0,0 +1,21 @@
+<div class="container stopka">
+	<div class="row">
+		<div class="col-md-5 col-xs-12">
+			<ul class="list-unstyled small">
+				<li>Copyright <?php echo date(Y); ?> <a href="http://goodstat.com.pl/" target="_blank" data-toggle="tooltip" data-placement="right" title="Darmowy system do zliczania statystyk stron www">Statystyki GoodStat</a> - strona domowa projektu</li>
+				<li><a href="mailto:kontakt@goodstat.com.pl">kontakt@goodstat.com.pl</a></li>
+				<li><a href="" role="button" class="popover-test" data-toggle="modal" data-target="#regulamin_stopka" title="Regulamin GoodStat-u">Regulamin</a> Systemu GoodStat</li>
+			</ul>
+			
+			
+		</div>
+
+		<div class="hidden-xs d-none d-md-block col-md-7 col-xs-12 logo-baner small">
+			
+		</div>
+	</div>
+</div>
+
+<?php
+	include('regulamin_stopka.php');
+?>
